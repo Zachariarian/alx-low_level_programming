a4c7439ca6da767273edbf06e840d2428664fb70
Zachariarian <zachariarian@gmail.com>
2022-05-24 03:42:45 -0700

    this

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,8 +2,8 @@
 void __attribute__((constructor)) hare(void);
 /**
  * hare - Prints a string before the
- * 
- * main function is executed.
+ * main function
+ * is executed.
  */
 void hare(void)
 {
