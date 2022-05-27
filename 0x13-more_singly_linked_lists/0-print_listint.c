e5978d00c0857c53ecc1a3e78cd8535114c3481b
Zachariarian <zachariarian@gmail.com>
2022-05-27 01:24:50 -0700

    up

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,7 +2,7 @@
 /**
  * print_listint - prints all the elements of a linked list
  * @h: head of the list
- *
+ * element
  * Return: the number of nodes
  */
 size_t print_listint(const listint_t *h)
