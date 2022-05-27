86f474fc1dbef056f1d88930749f1d0c744bdd1e
Zachariarian <zachariarian@gmail.com>
2022-05-27 01:22:16 -0700

    up

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /**
- * print_listint - prints all the
- * elements of a linked list
+ * print_listint - prints all the elements of a linked list
  * @h: head of the list
+ *
  * Return: the number of nodes
  */
 size_t print_listint(const listint_t *h)
--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 /**
  * free_listint2 - frees a linked list
- * @head: pointer to the head of the list
+ * @head: pointer to the
+ * head of the list
  * Return: void
  */
 void free_listint2(listint_t **head)
