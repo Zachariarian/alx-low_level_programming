2f08d8d20cdc91d95b3035f79c59d692de437178
Zachariarian <zachariarian@gmail.com>
2022-05-27 01:41:15 -0700

    bend

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,7 +3,7 @@
 /**
  * print_listint - prints all the elements of a linked list
  * @h: head of the list
- * Return: the number of nodes
+ *Return: the number of nodes
  */
 
 size_t print_listint(const listint_t *h)
