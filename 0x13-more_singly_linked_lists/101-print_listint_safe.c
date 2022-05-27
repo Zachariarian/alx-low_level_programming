fbb186eb4b733a98975a46ab9b7958851d9af29e
Zachariarian <zachariarian@gmail.com>
2022-05-27 06:06:25 -0700

    heylo

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,10 +1,9 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- * looped_listint_len - Counts the number of unique nodes 
- * @head: A pointer to the head of the listint_t to check.
- * Return: If the list is not looped - 0.
- * Otherwise - the number of unique nodes in the list.
+ * looped_listint_len - Counts the number of unique nodes
+ * @h: the head of list
+ * Return: If list not looped - 0. Otherwise -number of unique nodes
  */
 size_t looped_listint_len(const listint_t *head)
 {
--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -2,7 +2,6 @@
 /**
  * free_listint_safe - thsi function free a list in safe mode
  * @h: the head of list
- * Description: this function free a string in a safe mode
  * Return: the size of the list
  */
 size_t free_listint_safe(listint_t **h)
