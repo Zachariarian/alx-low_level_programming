7f2673b89549114cf23e0df32c0c56c360164892
Zachariarian <zachariarian@gmail.com>
2022-07-07 02:25:22 -0700

    merlin

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,35 +1,17 @@
 #include "lists.h"
+#include <stdio.h>
 /**
- * print_dlistint - prints all the elements of a dlistint_t list.
- * @h: pointer to the head of the list
+ * print_dlistint - print all the elements of a dlistint_t list
+ * @h: pointer to the start of the linked list
  * Return: number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-int counter = 0;
-if (h == NULL)
-return (counter);
-while (h->prev != NULL)
-h = h->prev;
-while (h != NULL)
+size_t i;
+for (i = 0; h != NULL; i++)
 {
 printf("%d\n", h->n);
 h = h->next;
-counter++;
 }
-return (counter);
-size_t print_dlistint(const dlistint_t *h)
-{
-int counter = 0;
-if (h == NULL)
-return (counter);
-while (h->prev != NULL)
-h = h->prev;
-while (h != NULL)
-{
-printf("%d\n", h->n);
-h = h->next;
-counter++;
-}
-return (counter);
+return (i);
 }
--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,49 +1,37 @@
 #include "lists.h"
+#include <stdlib.h>
 /**
- * delete_dnodeint_at_index - a function that deletes the node at
- * index index of a dlistint_t linked list
- * @head: head node
- * @index:  the index of the node that should be deleted. Index starts at 0
- * Return: 1 if it succeeded, -1 if it failed
+ * delete_dnodeint_at_index - deletes a node at a specific index
+ * @head: double pointer to the linked list
+ * @index: index at which to delete node
+ * Return: 1 on success, -1 on failure
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-dlistint_t *curr_node = *head;
-size_t size = 0;
-if (!*head)
-{
+dlistint_t *current;
+unsigned int i;
+if (head == NULL || *head == NULL)
 return (-1);
-}
-while (curr_node)
+current = *head;
+if (index == 0)
 {
-size++;
-curr_node = curr_node->next;
-}
-if (size < index + 1)
+*head = current->next;
+if (current->next != NULL)
 {
-return (-1);
+current->next->prev = NULL;
 }
-curr_node = *head;
-if (!index)
-{
-*head = curr_node->next;
-if (curr_node->next)
-{
-curr_node->next->prev = NULL;
-}
-curr_node->next = NULL;
-free(curr_node);
+free(current);
 return (1);
 }
-while (index--)
+for (i = 0; i < index; i++)
 {
-curr_node = curr_node->next;
+if (current->next == NULL)
+return (-1);
+current = current->next;
 }
-curr_node->prev->next = curr_node->next;
-if (curr_node->next)
-{
-curr_node->next->prev = curr_node->prev;
-}
-free(curr_node);
+current->prev->next = current->next;
+if (current->next != NULL)
+current->next->prev = current->prev;
+free(current);
 return (1);
 }
