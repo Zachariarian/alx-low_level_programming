75f8a12470d114f82fb7129678c2ca7a41bc9f19
Zachariarian <zachariarian@gmail.com>
2022-07-28 23:53:27 -0700

    0x1A-hash_tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,19 +6,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *ht;
-if (size < 1)
-return (NULL);
-ht = malloc(sizeof(hash_table_t));
-if (ht == NULL)
-return (NULL);
-ht->size = size;
-ht->array = calloc(size, sizeof(hash_node_t *));
-if (ht->array == NULL)
-{
-free(ht);
-return (NULL);
-}
-return (ht);
+	hash_table_t *ht;
+        if (size < 1)
+		return (NULL);
+        ht = malloc(sizeof(hash_table_t));
+        if (ht == NULL)
+		return (NULL);
+        ht->size = size;
+        ht->array = calloc(size, sizeof(hash_node_t *));
+        if (ht->array == NULL)
+        {
+                free(ht);
+                return (NULL);
+        }
+        return (ht);
 }
 
